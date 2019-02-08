fb8cb4994af11dac7f1a57801c8ddc4ae6b1eeb8
mtkachenko <maxim.tkachenko@wavecell.com>
2019-02-08 16:01:53 +0300

    added benchmarks project

---
+++ src/Mtk.CacheOnce.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+
+namespace Mtk.CacheOnce.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    [ConcurrencyVisualizerProfiler]
+    public class Benchmarks
+    {
+
+    }
+}
